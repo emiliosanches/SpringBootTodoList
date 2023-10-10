529cee6967bf1995f37734939acc6961a376537d
Emilio <emiliosneto13@gmail.com>
2023-10-10 16:58:24 -0300

    Aula2: Conexão com banco de dados usando Spring Data JPA

--- src/main/java/br/com/emiliosanches/todolist/user/UserController.java
+++ src/main/java/br/com/emiliosanches/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.emiliosanches.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.name);
-    System.out.println(userModel.username);
-    System.out.println(userModel.password);
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var existingUser = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (existingUser != null) {
+      System.out.println("Usuário já existe");
+      return ResponseEntity.status(400).body("Usuário já existe");
+    }
+
+    var savedUser = this.userRepository.save(userModel);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
   }
 }
