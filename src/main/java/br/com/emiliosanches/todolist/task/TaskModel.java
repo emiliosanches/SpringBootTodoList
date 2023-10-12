285fd2d4f44d341c2f28369bc8ff22d6deeb4dd0
Emilio Sanches <emilio.dev@hublocal.com.br>
2023-10-12 12:37:17 -0300

    Decorar TaskModel.createdAt com @CreationTimestamp

--- src/main/java/br/com/emiliosanches/todolist/task/TaskModel.java
+++ src/main/java/br/com/emiliosanches/todolist/task/TaskModel.java
@@ -3,6 +3,8 @@ package br.com.emiliosanches.todolist.task;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,7 +17,7 @@ public class TaskModel {
   @Id
   @GeneratedValue(generator = "UUID")
   private UUID id;
-  
+
   private String description;
 
   @Column(length = 50)
@@ -24,6 +26,9 @@ public class TaskModel {
   private LocalDateTime startsAt;
   private LocalDateTime endsAt;
   private String priority;
+  
+  @CreationTimestamp
   private LocalDateTime createdAt;
+  
   private UUID userId;
 }
