cbee22a36cad4082cf2568054dca75e6bd0f2557
Emilio Sanches <emilio.dev@hublocal.com.br>
2023-10-13 18:48:35 -0300

    Autenticação: retornar 401 em caso de username não encontrado

--- src/main/java/br/com/emiliosanches/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/emiliosanches/todolist/filter/FilterTaskAuth.java
@@ -24,6 +24,12 @@ public class FilterTaskAuth extends OncePerRequestFilter {
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
     var authorizationHeader = request.getHeader("Authorization");
+
+    if (authorizationHeader == null) {
+      response.sendError(401);
+      return;
+    }
+  
     var basicToken = authorizationHeader.substring("Basic".length()).trim();
 
     byte[] authDecoded = Base64.getDecoder().decode(basicToken);
